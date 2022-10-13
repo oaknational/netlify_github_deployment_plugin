13f5572cbe0983c8321fa43c51daeea1abc83e5c
Jim Cresswell <jim.cresswell@gmail.com>
2022-10-13 15:11:35 +0100

    chore: linting and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    node: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: ["eslint:recommended", "prettier"],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  plugins: ["prettier"],
+  rules: { "prettier/prettier": ["error"] },
+};
